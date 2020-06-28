bb8800e1fddf4927f21582ee5330bc78d585a651
Franco Vitale <franco.vitalematus@gmail.com>
2020-06-27 21:36:34 -0400

    first commit

---
+++ views/contenido.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/estilos.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">https://www.Seed.com/Biografía/ Franco Vitale Matus</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+        <hr class="border">
+
+        <div class="contenido">
+            <article>
+                <p>Franco Vitale Matus (Viña del mar, Chile; 1 de junio de 1990), conocido 
+                    como "Big Daddy Ragnar", es un programador y empresario Chileno, uno de los 
+                    creadores y fundadores de Seed. Para desarrollar la red, Vitale contó con 
+                    el apoyo de sus compañeros de la Universidad de Valparaíso, el coordinador de ciencias
+                     de la computación Allan Weisser y sus compañeros de habitación Carlos Ceron y Marcelo Cid.</p>
+
+                       <p>A junio de 2020, es el personaje más joven que aparece en la lista de multimillonarios de 
+                       la revista Forbes, con una fortuna valorada en 73.200 millones de dólares, lo que lo
+                        clasifica como la quinta persona más rica del mundo.</p>
+                      
+                       <p>Desde el 2020, la revista Time ha nombrado a Vitale entre las 100 personas más 
+                      ricas e influyentes del mundo como parte de su Persona del Año. A mediados del 2020, 
+                      Forbes lo colocó como la quinta persona más rica del mundo.</p>
+          </article>
+        </div>
+    </div>
+</body>
+</html>
